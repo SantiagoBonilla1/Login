63681b1224f76e6f38d94f6127d01591e38de209
SantiagoBonilla1 <santiagobonillaeguren@gmail.com>
2021-10-21 16:51:25 -0300

    Agregados cambios y corrección de validations

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python:3
+ENV PYTHONUNBUFFERED=1
+ENV PYTHONDONTWRITEBYTECODE=1
+RUN mkdir /code
+WORKDIR /code
+COPY requirements.txt /code/
+COPY requirements.txt /code/
+RUN python -m pip install -r requirements.txt
+RUN python -m pip install mysqlclient
+COPY . /code/
+
---
+++ Login/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpRequest
+from django.template import Template, Context
+from django.shortcuts import redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import *
+from django.contrib.auth.forms import PasswordChangeForm, PasswordResetForm
+from .forms import UserCreationForm
+import re
+
+def loguearse(request):
+    Logueo = AuthenticationForm()
+    if request.method == 'POST':
+        Logueo = AuthenticationForm(data=request.POST)
+        nameUser = request.POST.get('username')
+        namePassword = request.POST.get('password')
+        if Logueo.is_valid():
+            user = authenticate(request, username=nameUser, password=namePassword)
+            if user is not None:
+                login(request, user)
+                request.session['nombre'] = nameUser
+                return redirect('/Inicio/')
+            else:
+                return render(request, 'Login.html',{'form': Logueo})
+        else:
+            return render(request, 'Login.html',{'form': Logueo})
+    else:
+        return render(request, 'Login.html', {'form': Logueo})
+
+def Registrarse(request):
+    if request.method == 'POST':
+        Registrar = UserCreationForm(data=request.POST)
+        password = request.POST.get('password1')
+        if Registrar.is_valid():
+            Registrar.save()
+            return redirect('/Login/RegistroCompleto')
+        else:
+            messages = ""
+            return render(request, 'Register.html',{'form': Registrar, 'mensaje':messages})
+    else:
+        Registrar = UserCreationForm()
+        return render(request, 'Register.html', {'form': Registrar})
+    
+def Logout(request):
+    logout(request)
+    return redirect('/Login/')
+
+def Inicio(request):
+    return render(request, 'Inicio.html')
+
+def Login(request):
+    Logueo = AuthenticationForm()
+    return render(request, 'Login.html', {'form': Logueo})
+
+def redirect_view_Login(request):
+    return redirect('/Login/')
+
+def redirect_view_Register(request):
+    return redirect('/Registro/')
+
+def redirect_view_Inicio(request):
+    return redirect('/Inicio/')
