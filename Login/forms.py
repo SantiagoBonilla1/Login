d1c4f7fa40b36d4ccc1717cd2804d0497405305e
SantiagoBonilla1 <85138113+SantiagoBonilla1@users.noreply.github.com>
2021-10-28 22:38:02 -0300

    Update forms.py

--- Login/forms.py
+++ Login/forms.py
@@ -6,14 +6,15 @@ from django.contrib.auth.models import User
 class UserCreationForm(UserCreationForm):
     username = forms.CharField(max_length=25)
     email = forms.EmailField(required=True, label='Email')
-
+    
     class Meta:
         model = User
-        fields = ("username", "email", "password1", "password2")
+        fields = ("username", "email", "password1", "password2", "password_date")
 
     def save(self, commit=True):
         user = super(UserCreationForm, self).save(commit=False)
         user.email = self.cleaned_data["email"]
+        user.password_date = self.date.today()
         if commit:
             user.save()
         return user
